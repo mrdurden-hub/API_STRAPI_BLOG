bc074d3849188dad406b8d959cded728032854c3
mrdurden-hub <cmateus297@gmail.com>
2021-10-16 16:02:31 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed235fff2b60d705235a877dc3c49d90'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f497823b-b6f6-44f8-ac49-1eb689248bdb'
+};
